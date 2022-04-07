ceec3607c485bc60cc270802d89cb9094e408114
Kralj Stone <arkham1001@gmail.com>
2022-04-07 14:23:04 +0200

    returned line

--- Frontend/src/Components/Button.jsx
+++ Frontend/src/Components/Button.jsx
@@ -1,9 +1,9 @@
-const Button = ({ name, color, bgColorFn, deleteID }) => {
+const Button = ({ name, color, bgColorFn }) => {
   return (
     <button
       style={{ borderColor: `${color}`, color }}
       onClick={(e) => {
-        console.log(deleteID);
+        bgColorFn(color);
 
         fetch(
           "https://twitter-clone-b69f5-default-rtdb.europe-west1.firebasedatabase.app/colors.json",
